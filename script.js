c5d4986e968df5bcb8bece4d30c0e515f137fbd5
Anne Ensign <anne.ensign@gmail.com>
2021-03-25 12:46:23 -0400

    In process: changing code to OOJS to make legible

--- script.js
+++ script.js
@@ -1,191 +1,285 @@
-//* --------------------- MAKE 12 CARDS ---------------------
+// //* --------------------- MAKE 12 CARDS ---------------------
 
-//*  GLOBAL ARRAYS FOR CARDS
+// //*  GLOBAL ARRAYS FOR CARDS
 
-//  Images for memory game, 6 indivdual pictures (12 pairs)
-const images =  ["center / cover url(./imgs/image1.png)", 
-                "center / cover url(./imgs/image1.png)", 
-                "center / cover url(./imgs/image2.png)",
-                "center / cover url(./imgs/image2.png)",
-                "center / cover url(./imgs/image3.png)",
-                "center / cover url(./imgs/image3.png)",
-                "center / cover url(./imgs/image4.png)", 
-                "center / cover url(./imgs/image4.png)", 
-                "center / cover url(./imgs/image5.png)",
-                "center / cover url(./imgs/image5.png)",
-                "center / cover url(./imgs/image6.png)",
-                "center / cover url(./imgs/image6.png)"];
+// //  Images for memory game, 6 indivdual pictures (12 pairs)
+// const images =  ["center / cover url(./imgs/image1.png)", 
+//                 "center / cover url(./imgs/image1.png)", 
+//                 "center / cover url(./imgs/image2.png)",
+//                 "center / cover url(./imgs/image2.png)",
+//                 "center / cover url(./imgs/image3.png)",
+//                 "center / cover url(./imgs/image3.png)",
+//                 "center / cover url(./imgs/image4.png)", 
+//                 "center / cover url(./imgs/image4.png)", 
+//                 "center / cover url(./imgs/image5.png)",
+//                 "center / cover url(./imgs/image5.png)",
+//                 "center / cover url(./imgs/image6.png)",
+//                 "center / cover url(./imgs/image6.png)"];
 
-                // 12 Cards to append
-const cards =   ["card1", "card2", "card3", "card4", "card5", "card6", 
-                "card7", "card8", "card9", "card10", "card11", "card12"]
+//                 // 12 Cards to append
+// const cards =   ["card1", "card2", "card3", "card4", "card5", "card6", 
+//                 "card7", "card8", "card9", "card10", "card11", "card12"]
  
 
-//* FUNCTIONS TO MAKE CARDS                
-/**
- * addCard(index) : Appends a card to the board; appends front and back sides to a card
- * @param {number} index Current index number in iteration through cards array, makes unique IDs
- */
+// //* FUNCTIONS TO MAKE CARDS                
+// /**
+//  * addCard(index) : Appends a card to the board; appends front and back sides to a card
+//  * @param {number} index Current index number in iteration through cards array, makes unique IDs
+//  */
 
-function addCard (index) {
-  const board = document.getElementById("board");
-  const card = document.createElement('div');
-  card.setAttribute('id', 'card');
-    //  Set unique IDs to front, shift off images array for card pictures
-  const cardFront = document.createElement('div');
-  cardFront.setAttribute('class', 'card_front');
-  cardFront.setAttribute('id', 'card_front--' + index);
-  cardFront.style.background = images.shift();
+// function addCard (index) {
+//   const board = document.getElementById("board");
+//   const card = document.createElement('div');
+//   card.setAttribute('id', 'card');
+//     //  Set unique IDs to front, shift off images array for card pictures
+//   const cardFront = document.createElement('div');
+//   cardFront.setAttribute('class', 'card_front');
+//   cardFront.setAttribute('id', 'card_front--' + index);
+//   cardFront.style.background = images.shift();
 
-  const cardBack = document.createElement('div');
-  cardBack.setAttribute('id', 'card_back');
+//   const cardBack = document.createElement('div');
+//   cardBack.setAttribute('id', 'card_back');
 
-    //  Append sides to card first
-  card.appendChild(cardBack);
-  card.appendChild(cardFront);
-    //    Then append card to board
-  board.appendChild(card);
-}
-
-
-//  Iterate through cards array to append 12 cards to HTML
-function addCardsToBoard () {
-  for (let i = 0; i < cards.length; i++) {
-  addCard([i]);
-  }
-}
-
-addCardsToBoard();
-
-//==============================================================================
-
-//* --------------------- MANIPULATE CARDS ---------------------
+//     //  Append sides to card first
+//   card.appendChild(cardBack);
+//   card.appendChild(cardFront);
+//     //    Then append card to board
+//   board.appendChild(card);
+// }
 
 
-// SHUFFLE THE ORDER OF THE CARDS
+// //  Iterate through cards array to append 12 cards to HTML
+// function addCardsToBoard () {
+//   for (let i = 0; i < cards.length; i++) {
+//   addCard([i]);
+//   }
+// }
 
-const cardList = document.querySelectorAll('#card');
+// addCardsToBoard();
 
-function shuffle() {
-  cardList.forEach(card => {
-    let randomPosition = Math.floor(Math.random() * 12);
-    card.style.order = randomPosition;
-  });
-}
+// //==============================================================================
 
-shuffle();
-
-
-// FLIP CARDS ON CLICK
-
-const board = document.getElementById('board');
-const cardsFaceUp = [];
-let guesses = 0;
-let locked = false;
+// //* --------------------- MANIPULATE CARDS ---------------------
 
 
-//* --------------------- EVENT LISTENER ---------------------
+// // SHUFFLE THE ORDER OF THE CARDS
 
-cardList.forEach(card => {
-  //  Add event listener
-  card.addEventListener('click', function evaluate () {
-    //  Toggle to flip for each click
-    if (locked) return;
-    guesses++
-    card.classList.toggle('is-flipped');
+// const cardList = document.querySelectorAll('#card');
 
-    //  Flipped images to array to compare
-    let cardImage = card.childNodes[1].style.background;
-    cardsFaceUp.push(cardImage);
+// function shuffle() {
+//   cardList.forEach(card => {
+//     let randomPosition = Math.floor(Math.random() * 12);
+//     card.style.order = randomPosition;
+//   });
+// }
+
+// shuffle();
+
+
+// // FLIP CARDS ON CLICK
+
+// const board = document.getElementById('board');
+// const cardsFaceUp = [];
+// let guesses = 0;
+// let locked = false;
+
+
+// //* --------------------- EVENT LISTENER ---------------------
+// function addClickability () {
+// cardList.forEach(card => {
+//   //  Add event listener
+//   card.addEventListener('click', function evaluate () {
+//     //  Toggle to flip for each click
+//     if (locked) return;
+//     guesses++
+//     card.classList.toggle('is-flipped');
+
+//     //  Flipped images to array to compare
+//     let cardImage = card.childNodes[1].style.background;
+//     cardsFaceUp.push(cardImage);
     
-    checkForPairs();
-  })
-});
+//     checkForPairs();
+//   })
+// });
+// }
 
+// addClickability();
 
+// //* --------------------- CHECK FOR MATCHED PAIRS ---------------------
 
-//* --------------------- CHECK FOR MATCHED PAIRS ---------------------
+// //  Check for pairs on all flipped cards
+// function checkForPairs () {
+//     board.querySelectorAll('.is-flipped').forEach(() => {
 
-//  Check for pairs on all flipped cards
-function checkForPairs () {
-    board.querySelectorAll('.is-flipped').forEach(() => {
-
-      //  Is a match
-      if (guesses === 2 && cardsFaceUp[0] == cardsFaceUp[1]) {
-        locked = true;
-        let match = board.querySelectorAll('#card.is-flipped');
-        function addClass(){
-          for (let i = 0; i < match.length; i++) {
-            match[i].classList.add('disabled');
-            cardsFaceUp.pop();
-          }
-        }
-        addClass();
-        updateScore();
-        guesses = 0;
-        if (score === 6) {
-          setTimeout(function() {
-            showWinnerDiv();
-          }, 2000);
+//       //  Is a match
+//       if (guesses === 2 && cardsFaceUp[0] == cardsFaceUp[1]) {
+//         locked = true;
+//         let match = board.querySelectorAll('#card.is-flipped');
+//         function addClass(){
+//           for (let i = 0; i < match.length; i++) {
+//             match[i].classList.add('disabled');
+//             cardsFaceUp.pop();
+//           }
+//         }
+//         addClass();
+//         updateScore();
+//         guesses = 0;
+//         if (score === 6) {
+//           setTimeout(function() {
+//             showWinnerDiv();
+//           }, 2000);
           
-        };
-      locked = false;
-      //  Is not a match  
-      } else if (guesses === 2 && cardsFaceUp[0] !== cardsFaceUp[1]){
-        locked = true;
-        //  Much select all every time since document constantly changes
-        let noMatch = board.querySelectorAll('#card.is-flipped');
-        //  Iterate through node list andremove class
-        function removeClass(){
-          for (let i = 0; i < noMatch.length; i++) {
-            noMatch[i].classList.remove('is-flipped');
-          }
-        };
-        //  Set timeout function so they dont flip back immediately
-        for (let i = 0; i < noMatch.length; i++){
-          setTimeout(function(){
-            removeClass();
-            locked = false;
-          }, 2000); // 2 seconds
-          cardsFaceUp.pop();
-        };
-        guesses = 0;  
+//         };
+//       locked = false;
+//       //  Is not a match  
+//       } else if (guesses === 2 && cardsFaceUp[0] !== cardsFaceUp[1]){
+//         locked = true;
+//         //  Much select all every time since document constantly changes
+//         let noMatch = board.querySelectorAll('#card.is-flipped');
+//         //  Iterate through node list andremove class
+//         function removeClass(){
+//           for (let i = 0; i < noMatch.length; i++) {
+//             noMatch[i].classList.remove('is-flipped');
+//           }
+//         };
+//         //  Set timeout function so they dont flip back immediately
+//         for (let i = 0; i < noMatch.length; i++){
+//           setTimeout(function(){
+//             removeClass();
+//             locked = false;
+//           }, 2000); // 2 seconds
+//           cardsFaceUp.pop();
+//         };
+//         guesses = 0;  
 
-      //  If only one card chosen  
-      } else {
-        let onlyOneFlipped = board.querySelector('#card.is-flipped');
-        onlyOneFlipped.removeEventListener('click', true);
-        console.log(guesses);
-      }
-    })
+//       //  If only one card chosen  
+//       } else {
+//         // let onlyOneFlipped = board.querySelector('#card.is-flipped');
+//         // onlyOneFlipped.removeEventListener('click', true);
+//         console.log(guesses);
+//       }
+//     })
+//   }
+// //----------------------------------------------------------------------------------
+
+// //  Update score
+
+// let score = 0;
+// const won = document.getElementById('won');
+// const playAgainButton = document.getElementById('play_again');
+
+// function updateScore () {
+//   score++;
+// }
+
+// function showWinnerDiv() {
+//   won.style.display = "flex";
+// }
+
+// //----------------------------------------------------------------------------------
+
+
+
+// playAgainButton.addEventListener('click', () => {
+//   won.style.display = "none";
+//   for (let i = 0; i < cards.length; i++) {
+//     let node = document.getElementById('card');
+//     if (node.parentNode) {
+//       node.parentNode.removeChild(node);
+//     }
+//   }
+//   addCardsToBoard();
+//   shuffle();
+//   addClickability();
+// })
+
+
+//------------------------------------------
+
+
+class MemoryCard {
+  constructor(front, back) {
+    this.front = front;
+    this.back = back;
   }
-//----------------------------------------------------------------------------------
-
-//  Update score
-
-let score = 0;
-const won = document.getElementById('won');
-const playAgainButton = document.getElementById('play_again');
-
-function updateScore () {
-  score++;
 }
 
-function showWinnerDiv() {
-  won.style.display = "flex";
-}
-
-//----------------------------------------------------------------------------------
-
-
-
-playAgainButton.addEventListener('click', () => {
-  won.style.display = "none";
-  for (let i = 0; i < cards.length; i++) {
-    let node = document.getElementById('card');
-    if (node.parentNode) {
-      node.parentNode.removeChild(node);
+class Deck {
+  constructor() {
+    this.allCards = [];
+    this.makeCards();
+    this.shuffle();
+    this.render();
+    this.cardFlip();
+  }
+  makeCards() {
+    let cards =   ["card1", "card2", "card3", "card4", "card5", "card6", 
+                "card7", "card8", "card9", "card10", "card11", "card12"];
+    let images =  ["center / cover url(./imgs/image1.png)", 
+              "center / cover url(./imgs/image1.png)", 
+              "center / cover url(./imgs/image2.png)",
+              "center / cover url(./imgs/image2.png)",
+              "center / cover url(./imgs/image3.png)",
+              "center / cover url(./imgs/image3.png)",
+              "center / cover url(./imgs/image4.png)", 
+              "center / cover url(./imgs/image4.png)", 
+              "center / cover url(./imgs/image5.png)",
+              "center / cover url(./imgs/image5.png)",
+              "center / cover url(./imgs/image6.png)",
+              "center / cover url(./imgs/image6.png)"];
+    for (let i = 0; i < cards.length; i++) {  
+      let assignImage = images.shift(); 
+      let cardFront = assignImage;
+      let cardBack = "back";
+      MemoryCard.front = cardFront;
+      MemoryCard.back = cardBack;
+      this.allCards.push(new MemoryCard(cardFront, cardBack));
     }
   }
-  addCardsToBoard();
-})
+shuffle() {
+    const { allCards } = this;
+      for (let i = allCards.length - 1; i > 0; i--) {
+          const swapIndex = Math.floor(Math.random() * (i + 1)); 
+          const currentIndex = this.allCards[i];
+          const randomCardToSwap = this.allCards[swapIndex];
+          this.allCards[i] = randomCardToSwap;
+          this.allCards[swapIndex] = currentIndex;
+      };
+      return this;
+  }
+
+  render() { 
+    for (let i = 0; i < this.allCards.length; i++) {
+      const card = document.createElement('div');
+      card.classList = 'card';
+
+      const cardFront = document.createElement('div');
+      cardFront.classList = 'card_front';
+      let pairImage = this.allCards[i].front;
+      cardFront.style.background = pairImage
+
+      const cardBack = document.createElement('div');
+      cardBack.classList = 'card_back';
+      
+      card.appendChild(cardFront);
+      card.appendChild(cardBack);
+      document.getElementById('board').appendChild(card);
+
+      let pic = this.allCards[i].front;
+      cardFront.style.background = pic;
+    };
+    return this;
+  }
+  cardFlip() {
+    let cardClass = document.querySelectorAll(".card");
+    cardClass.forEach(card => {
+      card.addEventListener('click', () => {
+        card.classList.toggle('is-flipped');
+      })
+    })
+
+  }
+}
+const newDeal = new Deck;
+
+let locked = false;
